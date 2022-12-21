422c39310e9ba0a0bf42f2724c8934f7669b11c5
GiovanniMarcinari <gmarcinari@gmail.com>
2022-12-21 11:51:32 -0300

    Initial commit

---
+++ src/piscina/Main.java
@@ -0,0 +1,17 @@
+package piscina;
+
+import utils.Leitor;
+
+public class Main {
+    public static void main(String[] args) {
+        Leitor leitor = new Leitor();
+
+        double largura = leitor.lerDecimal("Digite a largura da piscina: ");
+        double comprimento = leitor.lerDecimal("Digite o comprimento da piscina: ");
+        double profundidade = leitor.lerDecimal("Digite a profundidade da piscina: ");
+
+        Piscina piscina = new Piscina(largura, comprimento, profundidade);
+        System.out.println(piscina);
+        System.out.printf("O volume da piscina é %.2f m³.", piscina.calcularVolume());
+    }
+}
